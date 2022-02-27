49a04f9b54abe96f4f8e4f9b428d8a6b0d036f11
Doan Sy Hung <hungdoansy@gmail.com>
2022-02-27 23:16:16 +0700

    refactor(game-row): simplify by using map

--- src/components/GameRow/index.js
+++ src/components/GameRow/index.js
@@ -3,28 +3,26 @@ import GameTile from "../GameTile";
 import "./GameRow.css";
 
 const GameRow = ({ letters }) => {
-  var cols = [];
-  var index = 0;
-  for (; index < letters.length; index++) {
-    const element = letters[index];
-    cols.push(
+  const tiles = letters.map((element, index) => {
+    return (
       <GameTile
         key={index}
         state={element.state}
         anim={element.anim}
         letter={element.text}
       />
-    );
-  }
+    )
+  })
+
   if (letters.length < 5) {
-    for (; index < 5; index++) {
-      cols.push(<GameTile key={index} />);
+    for (let index = 0; index < (5 - letters.length); index++) {
+      tiles.push(<GameTile key={index} />);
     }
   }
 
   return (
     <div className="row" data-animation="shake">
-      {cols.map((tile) => tile)}
+      {tiles}
     </div>
   );
 };
